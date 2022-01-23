a483ac3c93bc319469cda69f8b169e6ae8bd5ed5
Josh Samuels <joshsamuels04@gmail.com>
2022-01-22 22:15:43 -0500

    revert imports to cjs

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-import * as core from '@actions/core'
-import { render } from 'mustache';
-import { get } from 'axios'
+const core = require('@actions/core')
+const mustache = require('mustache')
+const axios = require('axios')
 
 try {
   const dataURL = core.getInput('resume-data-url')
@@ -13,11 +13,11 @@ try {
 
   // const template = await fetch("https://gist.github.com/joshmsamuels/14a51cb1be5e15a3e54b0dccf3b80286")
 
-  const template = await get("https://gist.github.com/joshmsamuels/951453f0aade3a132f6c8cbd91fd8a52")
+  const template = await axios.get("https://gist.github.com/joshmsamuels/951453f0aade3a132f6c8cbd91fd8a52")
 
   // console.log(render("Here is a sample template \{<%foo%>}", { foo: "MOOstashe" }, {}, mustacheConfig))
-  console.log(render(template, { foo: "MOOstashe" }, {}, mustacheConfig))
-  core.setOutput("filled", render(template, { foo: "MOOstashe" }, {}, mustacheConfig))
+  console.log(mustache.render(template, { foo: "MOOstashe" }, {}, mustacheConfig))
+  core.setOutput("filled", mustache.render(template, { foo: "MOOstashe" }, {}, mustacheConfig))
 
 } catch (error) {
   if (error instanceof Error) {
