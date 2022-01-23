eb6ca59de97ca3e88168537955c9622861926480
Josh Samuels <joshsamuels04@gmail.com>
2022-01-23 13:39:53 -0500

    test

--- src/populate-resume.js
+++ src/populate-resume.js
@@ -9,7 +9,7 @@ const fillResume = async() => {
     
     // const template = await fetch("https://gist.github.com/joshmsamuels/14a51cb1be5e15a3e54b0dccf3b80286")
     
-    const template = await axios.get("https://gist.github.com/joshmsamuels/951453f0aade3a132f6c8cbd91fd8a52")
+    const template = await axios.get("https://gist.githubusercontent.com/joshmsamuels/951453f0aade3a132f6c8cbd91fd8a52/raw/a1cdc747bb6cf59e0d6d2faa6cf8753d49482a2e/latex-sample.tex")
     
     console.log("Template", template)
     // console.log(render("Here is a sample template \{<%foo%>}", { foo: "MOOstashe" }, {}, mustacheConfig))
