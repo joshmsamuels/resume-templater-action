2036c6b340ff4414845570a7a74d225d02887986
Josh Samuels <joshsamuels04@gmail.com>
2022-01-23 13:31:14 -0500

    test

--- index.js
+++ index.js
@@ -1,23 +1,12 @@
 const core = require('@actions/core')
-const mustache = require('mustache')
-const axios = require('axios')
+const populateResume = require("src/populate-resume.js")
 
 try {
   const dataURL = core.getInput('resume-data-url')
   const templateURL = core.getInput('resume-template-url')
 
-  const mustacheConfig = {
-    tags: ["<%", "%>"],
-    escape: text => text
-  }
-
-  // const template = await fetch("https://gist.github.com/joshmsamuels/14a51cb1be5e15a3e54b0dccf3b80286")
-
-  const template = await axios.get("https://gist.github.com/joshmsamuels/951453f0aade3a132f6c8cbd91fd8a52")
-
-  // console.log(render("Here is a sample template \{<%foo%>}", { foo: "MOOstashe" }, {}, mustacheConfig))
-  console.log(mustache.render(template, { foo: "MOOstashe" }, {}, mustacheConfig))
-  core.setOutput("filled", mustache.render(template, { foo: "MOOstashe" }, {}, mustacheConfig))
+  populateResume()
+  .then(resume => core.setOutput("filled", resume))
 
 } catch (error) {
   if (error instanceof Error) {
---
+++ src/populate-resume.js
@@ -0,0 +1,18 @@
+const mustache = require('mustache')
+const axios = require('axios')
+
+const fillResume = async() => {    
+    const mustacheConfig = {
+      tags: ["<%", "%>"],
+      escape: text => text
+    }
+    
+    // const template = await fetch("https://gist.github.com/joshmsamuels/14a51cb1be5e15a3e54b0dccf3b80286")
+    
+    const template = await axios.get("https://gist.github.com/joshmsamuels/951453f0aade3a132f6c8cbd91fd8a52")
+    
+    // console.log(render("Here is a sample template \{<%foo%>}", { foo: "MOOstashe" }, {}, mustacheConfig))
+    console.log(mustache.render(template, { foo: "MOOstashe" }, {}, mustacheConfig))
+    
+    return mustache.render(template, { foo: "MOOstashe" }, {}, mustacheConfig)
+}
